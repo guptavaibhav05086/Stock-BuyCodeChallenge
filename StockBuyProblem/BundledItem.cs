4e2a4752d1bbfedf0d676d0ebcef9cc7a3fbac89
Vaibhav <guptavaibhav.05086@gmail.com>
2024-02-06 22:44:01 +0530

    Code Challange

---
+++ StockBuyProblem/BundledItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockBuyProblem
+{
+    public class BundledItem
+    {
+        public int partId {  get; set; }
+        public string? partName { get; set; }
+        public int partRequired { get; set; }
+        public List<BundledItem>? subBundlesList { get; set; }
+        public int partInvetory { get; set; }
+    }
+}
---
+++ StockBuyProblem/EFModels/Inventory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockBuyProblem.EFModels
+{
+    [Table("inventory")]
+    public class Inventory
+    {
+        [Key]
+        [Column("id ")]
+        public int Id { get; set; }
+       
+        [Column("bundle_Id")]
+        [ForeignKey("Bundles")]
+        public int BundleId { get; set; }
+        [Column("stock ")]
+        public int partInvetory { get; set; }
+
+    }
+}
